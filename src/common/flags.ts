e7a9115f819ddf357c0678513a28b7f68bdf718c
William Lahti <wilahti@gmail.com>
2022-02-12 23:07:57 -0800

    add F_ARROW_FUNCTION

--- src/common/flags.ts
+++ src/common/flags.ts
@@ -1,35 +1,36 @@
 import ts from "typescript";
 
-export const F_READONLY     = 'R';
-export const F_ABSTRACT     = 'A';
-export const F_PUBLIC       = '$';
-export const F_PRIVATE      = '#';
-export const F_PROTECTED    = '@';
-export const F_PROPERTY     = 'P';
-export const F_METHOD       = 'M';
-export const F_STATIC       = 'S';
-export const F_CLASS        = 'C';
-export const F_INTERFACE    = 'I';
-export const F_FUNCTION     = 'F';
-export const F_OPTIONAL     = '?';
-export const F_ASYNC        = 'a';
-export const F_EXPORTED     = 'e';
+export const F_READONLY       = 'R';
+export const F_ABSTRACT       = 'A';
+export const F_PUBLIC         = '$';
+export const F_PRIVATE        = '#';
+export const F_PROTECTED      = '@';
+export const F_PROPERTY       = 'P';
+export const F_METHOD         = 'M';
+export const F_STATIC         = 'S';
+export const F_CLASS          = 'C';
+export const F_INTERFACE      = 'I';
+export const F_FUNCTION       = 'F';
+export const F_ARROW_FUNCTION = '>';
+export const F_OPTIONAL       = '?';
+export const F_ASYNC          = 'a';
+export const F_EXPORTED       = 'e';
 
-export const T_UNION        = '|';
-export const T_INTERSECTION = '&';
-export const T_ANY          = '~';
-export const T_UNKNOWN      = 'U';
-export const T_VOID         = 'V';
-export const T_TUPLE        = 'T';
-export const T_ARRAY        = '[';
-export const T_THIS         = 't';
-export const T_GENERIC      = 'g';
+export const T_UNION          = '|';
+export const T_INTERSECTION   = '&';
+export const T_ANY            = '~';
+export const T_UNKNOWN        = 'U';
+export const T_VOID           = 'V';
+export const T_TUPLE          = 'T';
+export const T_ARRAY          = '[';
+export const T_THIS           = 't';
+export const T_GENERIC        = 'g';
 
-export type RtSimpleType<T> = { TΦ: T };
-export type RtVoidType     = RtSimpleType<typeof T_VOID>;
-export type RtUnknownType  = RtSimpleType<typeof T_UNKNOWN>
-export type RtAnyType      = RtSimpleType<typeof T_ANY>;
-export type RtThisType     = RtSimpleType<typeof T_THIS>;
+export type RtSimpleType<T>   = { TΦ: T };
+export type RtVoidType        = RtSimpleType<typeof T_VOID>;
+export type RtUnknownType     = RtSimpleType<typeof T_UNKNOWN>
+export type RtAnyType         = RtSimpleType<typeof T_ANY>;
+export type RtThisType        = RtSimpleType<typeof T_THIS>;
 
 export interface RtParameter {
     n : string;
