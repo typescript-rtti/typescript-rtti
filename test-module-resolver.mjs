e2beae2d0020792823ed169857a8370b27f90c44
William Lahti <wilahti@gmail.com>
2022-02-20 03:51:27 -0800

    updated module resolver

--- test-module-resolver.mjs
+++ test-module-resolver.mjs
@@ -1,5 +1,5 @@
 /**
- * @param {string} specifier
+ * @param {string} url
  * @param {{
  *   conditions: !Array<string>,
  *   parentURL: !(string | undefined),
@@ -8,22 +8,46 @@
  * @returns {Promise<{ url: string }>}
  */
 export async function resolve(url, context, defaultResolve) {
-    if (!global['moduleOverrides'] || !global['moduleOverrides'][url])
-        return defaultResolve(url, context, defaultResolve);
+    //console.log(`RTTI Test: ESM: Resolving '${url}'`);
 
-    let override = global['moduleOverrides'][url];
-    let source = `
-        const DATA = ${JSON.stringify(override)};
-        export default DATA;
-        ${Object.keys(override)
-            .filter(k => isNaN(k) && typeof override[k] !== 'function')
-            .map(k => `export const ${k} = DATA['${k}']`)
-            .join(";\n")}
-        ${Object.keys(override)
-            .filter(k => isNaN(k) && typeof override[k] === 'function')
-            .map(k => `export const ${k} = (${override[k].toString()})`)
-            .join(";\n")}
-    `;
+    let filename = url;
+    if (!filename.startsWith('data:')) {
+        if (!filename.endsWith('.js'))
+            filename = `${filename}.js`;
+        if (!filename.includes('/'))
+            filename = `./${filename}`;
+    }
+
+    let moduleOverrides = global['moduleOverrides'] || {};
+
+    if (!moduleOverrides[url] && !moduleOverrides[filename]) {
+        //console.log(`RTTI Test: ESM: Using default resolver.`);
+        return defaultResolve(url, context, defaultResolve);
+    }
+
+    //console.log(`RTTI Test: ESM: Using custom resolver.`);
+
+    let override = moduleOverrides[filename] || moduleOverrides[url];
+    let source;
+
+    if (typeof override === 'object') {
+        source = `
+            const DATA = ${JSON.stringify(override)};
+            export default DATA;
+            ${Object.keys(override)
+                .filter(k => isNaN(k) && typeof override[k] !== 'function')
+                .map(k => `export const ${k} = DATA['${k}']`)
+                .join(";\n")}
+            ${Object.keys(override)
+                .filter(k => isNaN(k) && typeof override[k] === 'function')
+                .map(k => `export const ${k} = (${override[k].toString()})`)
+                .join(";\n")}
+        `;
+    } else {
+        source = `
+            ${override}
+        `
+    }
 
     // console.log(`Overriding module "${url}"...`);
     // console.log(`Using source:`);
